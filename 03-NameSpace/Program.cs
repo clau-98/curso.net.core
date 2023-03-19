ebd1d831e9259414df9daee45eee2e668c679320
clau-98 <cla_dia98@hotmail.com>
2023-03-19 16:45:57 -0600

    Program CSharpTutorial

---
+++ 03-NameSpace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_NameSpace
+{
+    internal class Program
+    {
+     static void Main(string[] args)
+        {
+            School.Student student = new School.Student();
+            School.Course course = new School.Course();
+
+            School.Education.Student student1 = new School.Education.Student();
+            School.Education.Course course1 = new School.Education.Course();
+        }
+    }
+}
